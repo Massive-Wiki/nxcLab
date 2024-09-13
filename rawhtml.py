21e74a1b04846ea7bc429ef662dc9b5b8e7e698a
William L. Anderson <band@users.noreply.github.com>
2024-09-13 16:14:10 -0500

    add argparse for example input file

--- rawhtml.py
+++ rawhtml.py
@@ -7,12 +7,19 @@ simple raw html processing for mistletoe
 from mistletoe import Document
 from mistletoe.html_renderer import HtmlRenderer
 from mistletoe.span_token import SpanToken
-
 import re
+
 # Set up logging
 import logging, os
 logging.basicConfig(level=os.environ.get('LOGLEVEL','WARNING').upper())
 logger = logging.getLogger(__name__)
+
+#setup argparse
+import argparse
+def init_argparse():
+    parser = argparse.ArgumentParser(description='Experiment with raw html rendering')
+    parser.add_argument('--input', '-i', required=False, help='path to example/test markdown file')
+    return parser
 
 class RawHtml(SpanToken):
     pattern = re.compile(r"\{\< ([^>]*) \>\}")
@@ -31,16 +38,23 @@ class RawHtmlRenderer(HtmlRenderer):
         return template.format(inner=inner)
     
 def render_with_rawhtml(markdown):
-    logger.info(f"render_with_rawhtml markdown: {markdown}")
+    logger.debug(f"render_with_rawhtml markdown: {markdown}")
     with RawHtmlRenderer() as renderer:
         return renderer.render(Document(markdown))
     
 def main():
-    with open('rawhtml_examples.md', 'r', encoding="utf-8") as f:
+    argparser = init_argparse()
+    args = argparser.parse_args()
+    logger.debug(f"parsed args: {args}")
+
+    rawhtml_input = 'rawhtml_examples.md'
+    if args.input:
+        rawhtml_input = args.input
+    with open(rawhtml_input, 'r', encoding="utf-8") as f:
         markdown_text = f.read()
     logger.info(f"markdown text: {markdown_text}")
     rendered_html = render_with_rawhtml(markdown_text)
-    logger.info(f"the rendered html:\n{rendered_html}")
+    print(f"the rendered html:\n{rendered_html}")
     
 if __name__ == '__main__':
     exit(main())
---
+++ rawhtml_ex2.md
@@ -0,0 +1,22 @@
+# an alternate sample
+## Example 3
+
+{< span style="color:gray;" >}Is this gray?{< /span >}
+
+## Example 2
+
+{< p style="color:red;" >}
+This is red!
+{< /p >}
+
+# Example 1
+
+{< div class="navlink" >}
+
+- [Home](README)
+- [[Jankifiers]]
+
+{< /div >}
+
+-----
+Fin!
